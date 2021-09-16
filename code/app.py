c9087b033ce38e6e3ee63af6375787df1dba48f9
SmirnovaT <75753895+SmirnovaT@users.noreply.github.com>
2021-09-16 13:50:34 +0300

    add resourse

--- code/app.py
+++ code/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
-#from flask_restful import Resource, Api
-
+from flask_restful import Api
+from resources.publication import Publication, PublicationList
 
 app = Flask(__name__)
-#api = Api(app)
+api = Api(app)
 
+
+api.add_resource(Publication, '/publication/<string:title>')
+api.add_resource(PublicationList, '/publications')
 
 @app.route('/healthcheck')
 def healthcheck():
     return 'OK', 201
 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
