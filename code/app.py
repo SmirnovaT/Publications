a7f35b253d45b5e77b5d191927ec64b58985f37e
SmirnovaT <75753895+SmirnovaT@users.noreply.github.com>
2021-09-26 21:33:32 +0300

    add create_tables

--- code/app.py
+++ code/app.py
@@ -3,8 +3,13 @@ from flask_restful import Api
 from resources.publication import Publication, PublicationList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 api.add_resource(Publication, '/publication/<string:title>')
 api.add_resource(PublicationList, '/publications')
@@ -14,4 +19,6 @@ def healthcheck():
     return 'OK', 201
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
